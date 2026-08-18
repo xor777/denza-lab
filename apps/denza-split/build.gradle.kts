4ee26378173a59bf1b7e7e9ae7546271a3bdc95b
Dmitry Ukhanov <dima.ukhanov@gmail.com>
2026-08-18 12:11:10 +0300

    chore(denza-split): release 0.1.1

--- apps/denza-split/build.gradle.kts
+++ apps/denza-split/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.denza.split"
         minSdk = 33
         targetSdk = 33
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildFeatures {
